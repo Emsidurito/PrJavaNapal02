3317d46c406ccb4c2bbded09129164191ca46b6d
Alejandro Napal <aleejandronapal@gmail.com>
2022-05-29 12:50:51 +0200

    Treballant amb ordres Git i branques: Versió 1.1 del projecte prjavaCognom02

--- src/prjava02/PrjavaNapal02.java
+++ src/prjava02/PrjavaNapal02.java
@@ -19,6 +19,7 @@ public class PrjavaNapal02 {
     public static void main(String[] args) {
         System.out.println("versió 0.1 del projecte prjavaNapal02"); 	
         try {
+            System.out.println("Creació d'una branca del projecte prjavaNapal02");
             InetAddress adreça = InetAddress.getLocalHost();
             String hostname = adreça.getHostName();
             System.out.println("hostname="+hostname);
@@ -26,6 +27,7 @@ public class PrjavaNapal02 {
             System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
             System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
             System.out.println("Versió OS: " + System.getProperty("os.version"));
+            
         }
         catch (IOException e) {
 		System.out.println("Exception occurred");
